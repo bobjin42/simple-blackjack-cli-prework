775c2bbec747adb7199403a9431cd1b2ffb6176f
Yicheng Jin <bobjin42@gmail.com>
2018-06-07 19:11:56 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,11 +26,12 @@ def initial_round
   first_card = deal_card
   second_card = deal_card
   card_total = first_card + second_card
-  
+  display_card_total(card_total)
+  return card_total
 end
 
 def hit?
-  # code hit? here
+  prompt_user
 end
 
 def invalid_command

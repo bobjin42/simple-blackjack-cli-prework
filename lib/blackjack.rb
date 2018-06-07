5c773619efa20aa66c12e5aa1ebd98626eaa7dfc
Yicheng Jin <bobjin42@gmail.com>
2018-06-07 19:26:28 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,9 +54,10 @@ end
 def runner
   welcome
   card_total = initial_round
-  until card_total > 21 do
+  while card_total <= 21 do
     card_total = hit?(card_total)
     display_card_total(card_total)
+    
     
 end
     
